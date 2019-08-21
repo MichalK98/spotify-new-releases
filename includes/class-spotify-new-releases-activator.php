33381caf03ef06bceea85f81cd92c258ff8421c1
unknown <michal10203040@gmail.com>
2019-08-21 12:52:49 +0200

    First Commit

---
+++ includes/class-spotify-new-releases-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       chickencross.se
+ * @since      1.0.0
+ *
+ * @package    Spotify_New_Releases
+ * @subpackage Spotify_New_Releases/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Spotify_New_Releases
+ * @subpackage Spotify_New_Releases/includes
+ * @author     Michal Kurowski <michal10203040@gmail.com>
+ */
+class Spotify_New_Releases_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-spotify-new-releases-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       chickencross.se
+ * @since      1.0.0
+ *
+ * @package    Spotify_New_Releases
+ * @subpackage Spotify_New_Releases/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Spotify_New_Releases
+ * @subpackage Spotify_New_Releases/includes
+ * @author     Michal Kurowski <michal10203040@gmail.com>
+ */
+class Spotify_New_Releases_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
