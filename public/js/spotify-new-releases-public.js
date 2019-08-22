ed15899481d02122307a7cc1ffc305fbe8f7d968
Michal K <michal10203040@gmail.com>
2019-08-22 18:25:55 +0200

    JS target widget class/es

--- public/js/spotify-new-releases-public.js
+++ public/js/spotify-new-releases-public.js
@@ -2,7 +2,12 @@
 	'use strict';
 
 	$(document).ready(function () {
-
+		// Hitta klassen '.widget_spotify-new-releases' i DOM
+		// .each om det finns flera '.widget_spotify-new-releases' (flera widget)
+		// skicka med (index, parameter)
+		$('.widget_spotify-new-releases').each(function (i, widget) {
+			console.log("Widget" + i + ":", widget);
+		});
 	});
 
 })(jQuery);
