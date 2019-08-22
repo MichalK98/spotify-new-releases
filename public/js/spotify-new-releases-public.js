1b09b8aaeb1d4d5b6f7fd4e32c6c1d35d60024a8
Michal K <michal10203040@gmail.com>
2019-08-22 18:35:48 +0200

    JS .post .done .fail

--- public/js/spotify-new-releases-public.js
+++ public/js/spotify-new-releases-public.js
@@ -7,6 +7,18 @@
 		// skicka med (index, parameter)
 		$('.widget_spotify-new-releases').each(function (i, widget) {
 			console.log("Widget" + i + ":", widget);
+
+			// Vi ska posta NÅGONSTANS, skicka med någon typ av data i brevet som vi skickar till NÅGONSTANS
+			$.post(
+				'somedata', // URL to POST to // mottagare på ett brev // michal@gmail.com
+				{} // DATA to send to the URL // innehållet i brevet // Hej du är bäst. MVH Michal
+			).done(function (response) {
+				// Om det gick bra kör denna funktionen
+				console.log("Got response", response);
+			}).fail(function (error) {
+				// Om det gick fel kör denna funktionen
+				console.log("Something went wrong", error);
+			});
 		});
 	});
 
